483e6f434ff25c8922eac95bcb36ef185aa6849b
Jeroen Meulemeester <jeroen.meulemeester@gmail.com>
2014-07-07 21:12:50 +0200

    Updated install script

--- install.sh
+++ install.sh
@@ -1,5 +1,23 @@
-#!/bin/sh
+#!/bin/bash
 
-echo "Linking user bin folder"
-rm -rf ~/bin
-ln -sf ~/dotfiles/bin ~/bin
+set -e
+set -u
+
+function linkFolder() {
+        local src=${1}
+        local dst=${2}
+        echo "Creating link [${dst}] to folder [${src}]"
+        rm -rf ${dst}
+        ln -sf ${src} ${dst}
+}
+
+function linkFile() {
+	local src=${1}
+	local dst=${2}
+	echo "Creating link [${dst}] to file [${src}]"
+	rm -rf ${dst}
+	ln -sf ${src} ${dst}
+}
+
+linkFolder ~/dotfiles/bin ~/bin
+linkFile ~/dotfiles/i3status.conf ~/.i3status.conf
